aa20bfa10863bca314395d0fc8a2b7615dce2bb6
kirthikagurumurthy <51313962+kirthikagurumurthy@users.noreply.github.com>
2019-08-02 22:58:36 +0530

    Update WorkloadStoragePlanetLabRead.java

--- sources/org/cloudbus/cloudsim/WorkloadStoragePlanetLabRead.java
+++ sources/org/cloudbus/cloudsim/WorkloadStoragePlanetLabRead.java
@@ -15,7 +15,7 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 	/** The data (5 min * 288 = 24 hours). */
 	private final double[] data; 
 	
-	private final  double[] speed; //size per read.
+	private final  double[] speed; //read mbps
 	
 	private double Sizeread = 0;
 	
@@ -36,18 +36,19 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 		speed = new double[289];
 		setSchedulingInterval(schedulingInterval);
 		BufferedReader input = new BufferedReader(new FileReader(inputPath));
-		int n = data.length;
-	
-		for (int i = 0; i < n - 1; i++) {
+		int i = 0;
+		String line;
+		while((line = input.readLine()) != null) {
 			try {
-			int length = ((input.readLine()).split(" ",-1)).length;
+			int length = line.split(" ",-1).length ;
+			
 			String[] workloadvalues = new String[length];
 			if(length <= 1) {
 				data[i] = 0;
 				speed[i] = 0;
 			}
 			else {
-			workloadvalues = (input.readLine()).split(" ",-1);
+			workloadvalues = line.split(" ",-1);
 			data[i] = Integer.valueOf(workloadvalues[4]);
 			speed[i] = Integer.valueOf(workloadvalues[6]);
 			
@@ -57,11 +58,12 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 				data[i]=0;
 				speed[i] = 0;
 			}
+			i++;
 		}
-		data[n - 1] = data[n - 2];
-		speed[n-1] = speed[n-2];
+
 		input.close();
 	}
+	
 		
 	
 	
@@ -80,7 +82,7 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 		data = new double[dataSamples];
 		speed = new double[dataSamples];
 		BufferedReader input = new BufferedReader(new FileReader(inputPath));
-		int n = data.length;
+		int n = 11;
 	
 		for (int i = 0; i < n - 1; i++) {
 			try {
@@ -115,34 +117,34 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 	public double getSize(double time, long fileSize)  // size read at a given time.
 	{	
 		if(fileSize>0) {
-		if (time % getSchedulingInterval() == 0) {
-		//	int fileSize = fileSize;
-			//file.setFileSize(fileSize -(new Double(data[(int) time / (int) getSchedulingInterval()]*percentage*fileSize )).intValue());
-			if(data[(int) time / (int) getSchedulingInterval()]*speed[(int) time / (int) getSchedulingInterval()]<fileSize) {
+			if (time % getSchedulingInterval() == 0) {
+		
+			if(data[(int) time / (int) getSchedulingInterval()]*speed[(int) time / (int) getSchedulingInterval()]<fileSize)
+			{
 				return data[(int) time / (int) getSchedulingInterval()]*speed[(int) time / (int) getSchedulingInterval()];
 			}
 			else {
 				return fileSize;
-			}
-		}
+			     }
+													  }
 		
 		int time1 = (int) Math.floor(time / getSchedulingInterval());
-	//	int fileSize = fileSize;
-		//file.setFileSize(fileSize -(new Double(data[time1]*percentage*fileSize )).intValue());
+	
 		if(data[time1]*speed[time1] < fileSize) {
 			this.Sizeread = data[time1]*speed[time1]; // gives the size read. .
 			return Sizeread;
-		}
+												}
 		else {
 			return fileSize;
-		}
-		}
+			 }
+												}
 		else {
 			return 0;
-		}
+			 }
 	}
 	
 	
+	@Override
 	public int  getNumber(double time, long fileSize) {
 		if (fileSize < 0 ) {	
 			return 0;
@@ -155,27 +157,28 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 		return numberOfreads;
 	}
 	
-	//Gets total number of writes .  SizePerWrite can be made as a constant value for a particular storage type.
+	//Gets total number of reads
+	@Override
 	public int getTotalNumber(long fileSize ) {
 		double size=0;
-		double now = 0; //initial value of total number of reads
+		double nor = 0; //initial value of total number of reads
 		for(int i=0; i<data.length;i++) {
 			double numberOfreads = data[i];
 			double ReadSpeed = speed[i];
 			if(Math.floor(size + numberOfreads*ReadSpeed) < fileSize) { //checking if the writing again makes the size read greater than the file size
-			now = now + numberOfreads;
+			nor = nor + numberOfreads;
 			size = size + numberOfreads*ReadSpeed;
 			}
 			else if(size !=  fileSize){
-				now = now+1; // If the above condition fails, then adding one more read to minimize error.
+				nor = nor+1; // If the above condition fails, then adding one more read to minimize error.
 			}
 			else {
 				break;
 			}
 		}
 		@SuppressWarnings("deprecation")
-		Double tnw = new Double(now);
-		int totalNumberOfreads = tnw.intValue();
+		Double tnr = new Double(nor);
+		int totalNumberOfreads = tnr.intValue();
 		return totalNumberOfreads;
 		
 	}
