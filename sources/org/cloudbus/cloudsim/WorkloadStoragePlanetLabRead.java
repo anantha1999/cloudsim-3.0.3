2d97db188a8c993aa98c60abd5814500a2079e97
anantha1999 <49026944+anantha1999@users.noreply.github.com>
2019-08-31 14:13:35 +0530

    Update WorkloadStoragePlanetLabRead.java

--- sources/org/cloudbus/cloudsim/WorkloadStoragePlanetLabRead.java
+++ sources/org/cloudbus/cloudsim/WorkloadStoragePlanetLabRead.java
@@ -9,19 +9,18 @@ import java.io.IOException;
  */
 public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 	
+	
 	/** The scheduling interval. */
 	private double schedulingInterval;
 
 	/** The data (5 min * 288 = 24 hours). */
 	private final double[] data; 
 	
-	private final  double[] speed; //read mbps
+	//private final double[] speed; //write mbps
 	
-	private double Sizeread = 0;
+	private double SizeRead = 0;
 	
-	
-	
-	private int numberOfreads;
+	private int numberOfReads;
 	/**
 	 * Instantiates a new utilization model PlanetLab.
 	 * 
@@ -33,7 +32,7 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 			throws NumberFormatException,
 			IOException {
 		data = new double[289];
-		speed = new double[289];
+		//speed = new double[289];
 		setSchedulingInterval(schedulingInterval);
 		BufferedReader input = new BufferedReader(new FileReader(inputPath));
 		int i = 0;
@@ -45,25 +44,29 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 			String[] workloadvalues = new String[length];
 			if(length <= 1) {
 				data[i] = 0;
-				speed[i] = 0;
+				//speed[i] = 0;
 			}
 			else {
 			workloadvalues = line.split(" ",-1);
 			data[i] = Integer.valueOf(workloadvalues[4]);
-			speed[i] = Integer.valueOf(workloadvalues[6]);
+			//speed[i] = Integer.valueOf(workloadvalues[5]);
 			
 			}
 			}
 			catch(Exception e) {
 				data[i]=0;
-				speed[i] = 0;
+				//speed[i] = 0;
 			}
 			i++;
+			
 		}
-
+		
+		
+		
+		//data[i - 1] = data[i - 2];
+		//speed[i-1] = speed[i -2];
 		input.close();
 	}
-	
 		
 	
 	
@@ -79,10 +82,10 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 			throws NumberFormatException,
 			IOException {
 		setSchedulingInterval(schedulingInterval);
+		//speed = new double[dataSamples];
 		data = new double[dataSamples];
-		speed = new double[dataSamples];
 		BufferedReader input = new BufferedReader(new FileReader(inputPath));
-		int n = 11;
+		int n = data.length;
 	
 		for (int i = 0; i < n - 1; i++) {
 			try {
@@ -90,22 +93,21 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 			String[] workloadvalues = new String[length];
 			if(length <= 1) {
 				data[i] = 0;
-				speed[i] = 0;
+				//speed[i] = 0;
 			}
 			else {
 			workloadvalues = (input.readLine()).split(" ",-1);
 			data[i] = Integer.valueOf(workloadvalues[4]);
-			speed[i] = Integer.valueOf(workloadvalues[6]);
-			
+			//speed[i] = Integer.valueOf(workloadvalues[5]);
 			}
 			}
 			catch(Exception e) {
 				data[i]=0;
-				speed[i] = 0;
+				//speed[i] = 0;
 			}
 		}
 		data[n - 1] = data[n - 2];
-		speed[n-1] = speed[n-2];
+		//speed[n-1] = speed[n-2];
 		input.close();
 	}
 
@@ -114,35 +116,34 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 	 * @see cloudsim.power.UtilizationModel#getUtilization(double)
 	 */
 	@Override
-	public double getSize(double time, long fileSize)  // size read at a given time.
+	/*public double getSize(double time, long fileSize)  // size written at a given time.
 	{	
 		if(fileSize>0) {
-			if (time % getSchedulingInterval() == 0) {
+		if (time % getSchedulingInterval() == 0) {
 			if(data[(int) time / (int) getSchedulingInterval()]*speed[(int) time / (int) getSchedulingInterval()]<fileSize) {
 				return data[(int) time / (int) getSchedulingInterval()]*speed[(int) time / (int) getSchedulingInterval()];
 			}
 			else {
 				return fileSize;
-				 }
-													  }
+			}
+		}
 		
 		int time1 = (int) Math.floor(time / getSchedulingInterval());
-	
 		if(data[time1]*speed[time1] < fileSize) {
-			this.Sizeread = data[time1]*speed[time1]; // gives the size read. .
-			return Sizeread;
-												}
+			this.SizeWritten = data[time1]*speed[time1]; // gives the size written. .
+			return SizeWritten;
+		}
 		else {
 			return fileSize;
-			 }
-												}
+		}
+		}
 		else {
 			return 0;
-			 }
+		}
 	}
 	
-	
 	@Override
+	// returns number of writes
 	public int  getNumber(double time, long fileSize) {
 		if (fileSize < 0 ) {	
 			return 0;
@@ -151,34 +152,47 @@ public class WorkloadStoragePlanetLabRead implements WorkloadStorage {
 		int time1 = (int) Math.floor(time / getSchedulingInterval());
 		@SuppressWarnings("deprecation")
 		Double NoW = new Double(data[time1]);
-		int numberOfreads = NoW.intValue();
-		return numberOfreads;
-	}
+		int numberOfWrites = NoW.intValue();
+		return numberOfWrites;
+	}*/
 	
-	//Gets total number of reads
-	@Override
-	public int getTotalNumber(long fileSize ) {
+	//Gets total number of writes 
+	/*@Override
+	public int getTotalNumber( long fileSize ) {
 		double size=0;
-		double nor = 0; //initial value of total number of reads
+		double now = 0; //initial value of total number of writes
 		for(int i=0; i<data.length;i++) {
-			double numberOfreads = data[i];
-			double ReadSpeed = speed[i];
-			if(Math.floor(size + numberOfreads*ReadSpeed) < fileSize) { //checking if the writing again makes the size read greater than the file size
-			nor = nor + numberOfreads;
-			size = size + numberOfreads*ReadSpeed;
+			double numberOfWrites = data[i];
+			double WriteSpeed = speed[i];
+			
+			if(Math.floor(size + numberOfWrites*WriteSpeed) < fileSize) { //checking if the writing again makes the size written greater than the file size
+			now = now + numberOfWrites;
+			size = size + numberOfWrites*WriteSpeed;
 			}
-			else if(size !=  fileSize){
-				nor = nor+1; // If the above condition fails, then adding one more read to minimize error.
+			else if(size != fileSize) {
+				now = now+1;
+				break;
 			}
 			else {
 				break;
 			}
 		}
 		@SuppressWarnings("deprecation")
-		Double tnr = new Double(nor);
-		int totalNumberOfreads = tnr.intValue();
-		return totalNumberOfreads;
+		Double tnw = new Double(now);
+		int totalNumberOfWrites = tnw.intValue();
+		return totalNumberOfWrites;
 		
+	}*/
+	
+	public int getTotalNumber() {
+		int totalReads = 0;
+		
+		for(int i=0; i<data.length;i++) {
+			Double NoR = new Double(data[i]);
+			int readValue = NoR.intValue();
+			totalReads = totalReads + readValue;
+		}
+		return totalReads;
 	}
 
 	/**
